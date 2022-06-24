3ff1596cbdcc902bfbd70c31f239bda26e056b82
DJUM <djum25@gmail.com>
2022-06-24 09:23:15 +0000

    display counted sales and uncounted sales

--- lib/page/shopWidgets/sale_news.dart
+++ lib/page/shopWidgets/sale_news.dart
@@ -5,6 +5,7 @@ import 'package:digital_farm_app/utils/sale.dart';
 import 'package:digital_farm_app/utils/service.dart';
 import 'package:digital_farm_app/widget/reverse_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -20,12 +21,15 @@ class SaleNewsPage extends StatefulWidget {
 class _SaleNewsPageState extends State<SaleNewsPage> {
   bool load = true;
   List<Sale> sales = [];
+  List<Sale> allSales = [];
   late DateFormat dateFormat;
   late DateFormat timeFormat;
   num priceTotal = 0;
   num accountTotal = 0;
   num advanceTotal = 0;
+  String reimburse = "0";
   String openAmount = '0';
+  bool all = false;
 
   @override
   void initState() {
@@ -45,18 +49,35 @@ class _SaleNewsPageState extends State<SaleNewsPage> {
           backgroundColor: Colors.white,
           elevation: 0.0,
           title: ClipRect(child: Image.asset('images/logoFarm.gif',width: 60.0,height: 60.0,)),
-          actions: <Widget>[ IconButton(icon: const Icon(Icons.money_outlined),onPressed: () {
-            showDialog(
+        )),
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.2,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.money_outlined,color: Colors.green,),
+              label: "Versement",
+              labelStyle: TextStyle(color: Colors.green),
+              onTap:() => showDialog(
             context: context,
             builder: (_) {
-              return ReverseForm((advanceTotal.toInt() + int.parse(openAmount)).toString());
-            },barrierDismissible: false);
-          })],
-        )),
+              return ReverseForm((advanceTotal.toInt() + int.parse(openAmount) +int.parse(reimburse)).toString());
+            },barrierDismissible: false)
+            ),SpeedDialChild(
+              child: Icon(Icons.list_alt_outlined,color: Colors.green),
+              label: all ? "Vente non versé" : "Tous les ventes",
+              labelStyle: TextStyle(color: Colors.green),
+              onTap: () => setState(() {
+                all = !all;
+              })
+            )
+          ],
+        ),
         body: Container( 
           height: MediaQuery.of(context).size.height - 62,
           decoration: BoxDecoration(image: DecorationImage(image: AssetImage("images/shop.jpg"),fit: BoxFit.cover,),),
-          child: load? Center(child: Image.asset('images/loading.gif',width: 300.0,height: 300.0,),) : layout(),
+          child: load? Center(child: Image.asset('images/loading.gif',width: 300.0,height: 300.0,),) : all ? itemListAll() :layout(),
     ));
   }
 
@@ -85,14 +106,17 @@ class _SaleNewsPageState extends State<SaleNewsPage> {
                     style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),  
         DataColumn( numeric: true,label: Text('Dette',  
                     style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
-                    DataColumn( numeric: true,label: Text('Etat ouverture',  
+        DataColumn( numeric: true,label: Text('Etat ouverture',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+        DataColumn( numeric: true,label: Text('Remboursement',  
                     style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
         ],
         rows: [DataRow(cells: [
               DataCell(Text(priceTotal.toString(),style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
               DataCell(Text(advanceTotal.toString(),style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
               DataCell(Text(accountTotal.toString(),style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
-               DataCell(Text(openAmount,style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+              DataCell(Text(openAmount,style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+              DataCell(Text(reimburse,style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
         ])],),
     );
   }
@@ -136,21 +160,63 @@ class _SaleNewsPageState extends State<SaleNewsPage> {
                 ])).toList())));
   }
 
+    Widget itemListAll(){
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 7.5), 
+      scrollDirection: Axis.horizontal,
+      child:SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+      child : DataTable(
+        decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.6,),borderRadius: BorderRadius.all(Radius.circular(15))),
+        dataTextStyle: TextStyle(color: Colors.white),
+        headingTextStyle: TextStyle(color: Colors.green),
+      //border: TableBorder.all(color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15))),
+      columns: 
+      [  
+        DataColumn(label: Text('Produit',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold) )),  
+        DataColumn( numeric: true, label: Text('Quantité',
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),  
+        DataColumn( numeric: true,label: Text('Montant total',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+        DataColumn( numeric: true,label: Text('Montant recu',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+        DataColumn( numeric: true,label: Text('Restant',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+        DataColumn(label: Text('Caissier',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+        DataColumn(label: Text('Date',  
+                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),  
+        ], 
+     rows:allSales.map((sale) => DataRow(cells: [  
+                  DataCell(Text(sale.product,style: TextStyle(fontSize: 19, fontStyle: FontStyle.italic),)),
+                  DataCell(Text(sale.quantity.toString())),
+                  DataCell(Text(sale.price.toString(),style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold))),
+                  DataCell(Text(sale.advance.toString())),
+                  DataCell(Text(sale.account.toString())),
+                  DataCell(Text(sale.user.toString())),
+                  DataCell(Text(formatDate(sale.date))),
+                ])).toList())));
+  }
+
   Future<void> getNews() async{
     var res = await CallApi().getData("/api/v1/cashier/sale/news/"+widget.id.toString());
     if(res.statusCode == 401){
       CallApi().logOut(context);
     }else{
     var body = jsonDecode(utf8.decode(res.bodyBytes));
-    print(body['objectArray']);
     if (body['success']) {
       SharedPreferences localStorage = await SharedPreferences.getInstance();
       for (var item in body['objectArray']) {
         sales.add(Sale.fromJson(item));
       }
+      for (var item in body['objectArrayChild']) {
+        allSales.add(Sale.fromJson(item));
+      }
       if(body['objectArray'].length > 0)
         setState(() {
           openAmount = localStorage.getString("cash")!;
+          reimburse = localStorage.getString("reimburse")!;
           priceTotal = sales.map((e) => e.price).toList().reduce((value,element)=>value+element);
           accountTotal = sales.map((e) => e.account).reduce((value,element)=>value+element);
           advanceTotal = sales.map((e) => e.advance).reduce((value,element)=>value+element);
@@ -159,6 +225,7 @@ class _SaleNewsPageState extends State<SaleNewsPage> {
         else
           setState(() {
             openAmount = localStorage.getString("cash")!;
+            reimburse = localStorage.getString("reimburse")!;
             load = false;
           });
     }
